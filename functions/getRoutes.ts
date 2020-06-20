84472ad192c227d788a29df5ce783898125cf856
Phumrapee Limpianchop <git@rayriffy.com>
2020-06-20 17:53:29 +0900

    chore: getRoutes

--- functions/getRoutes.ts
+++ functions/getRoutes.ts
@@ -1,3 +1,19 @@
 import { getQuery } from './getQuery'
 
-export const getRoutes = async () => {}
+import { data } from '../content/data'
+
+export const getRoutes = async () => {
+  return await Promise.all(
+    data.map(async data => {
+      const rawQuery = await getQuery(data.id)
+
+      return {
+        route: '/anime/' + data.id,
+        payload: {
+          ...data,
+          raw: rawQuery,
+        },
+      }
+    })
+  )
+}
